bfd85b180c8a33f8d09eb0b224fb69fae458c03a
tahmedofficial <tanvirofcuo@gmail.com>
2024-06-01 00:36:27 +0600

    added navbar

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,9 +1,34 @@
+import { RiMenu2Fill } from "react-icons/ri";
 
 const Navbar = () => {
+    
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <div className="navbar bg-base-100 md:w-5/6 mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                            <RiMenu2Fill className="h-5 w-5 text-black" />
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a>Homepage</a></li>
+                            <li><a>Portfolio</a></li>
+                            <li><a>About</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="navbar-center">
+                    <h1 className="text-3xl font-semibold">Study<span className="text-rose-600">Hive</span></h1>
+                </div>
+                <div className="navbar-end">
+                    <div className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Profile" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 };
 
