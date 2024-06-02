7372e871596bc023ac845e55437815b07e163365
tahmedofficial <tanvirofcuo@gmail.com>
2024-06-02 16:58:24 +0600

    finish study session

--- src/Pages/StudySession/StudySession.jsx
+++ src/Pages/StudySession/StudySession.jsx
@@ -1,3 +1,4 @@
+import SessionCart from "../../Components/SessionCart/SessionCart";
 import useAuth from "../../Hooks/useAuth";
 
 const StudySession = () => {
@@ -5,8 +6,12 @@ const StudySession = () => {
     const { courses } = useAuth();
 
     return (
-        <div>
-
+        <div className="md:w-5/6 mx-auto px-4 mt-16">
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                {
+                    courses.slice(0, 5).map(course => <SessionCart key={course._id} session={course}></SessionCart>)
+                }
+            </div>
         </div>
     );
 };
