d8efdde48d7c44ce83074c9584148f57cf2f08b2
tahmedofficial <tanvirofcuo@gmail.com>
2024-06-06 22:00:47 +0600

    finish delete function

--- src/Pages/Dashboard/AdminDashboard/AllStudySession/ApprovedSessions.jsx
+++ src/Pages/Dashboard/AdminDashboard/AllStudySession/ApprovedSessions.jsx
@@ -1,15 +1,41 @@
 import PropTypes from "prop-types";
+import Swal from "sweetalert2";
+import useAuth from "../../../../Hooks/useAuth";
+import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
 
 const ApprovedSessions = ({ session, index, reload }) => {
+
+    const { reload: loading, sweetMessage } = useAuth();
+    const axiosSecure = useAxiosSecure();
 
     const { _id, image, tutorName, tutorEmail, registrationFee, status } = session;
 
     const handleUpdate = (id) => {
         console.log(id);
     }
+
     const handleDelete = (id) => {
-        reload();
-        console.log(id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Delete"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/courses/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            reload();
+                            loading();
+                            sweetMessage("Successfully Delete");
+                        }
+                    })
+            }
+        });
+
     }
 
     return (
@@ -26,8 +52,8 @@ const ApprovedSessions = ({ session, index, reload }) => {
             <td>{tutorEmail}</td>
             <td>${registrationFee}</td>
             <td>{status}</td>
-            <td><button onClick={() => handleUpdate(_id)} className="btn btn-sm bg-green-500 text-white">Approved</button></td>
-            <td><button onClick={() => handleDelete(_id)} className="btn btn-sm bg-red-600 text-white">Reject</button></td>
+            <td><button onClick={() => handleUpdate(_id)} className="btn btn-sm bg-green-500 text-white">Update</button></td>
+            <td><button onClick={() => handleDelete(_id)} className="btn btn-sm bg-red-600 text-white">Delete</button></td>
         </>
     );
 };
